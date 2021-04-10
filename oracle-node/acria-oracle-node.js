4ec9c7915418e7b1527dfc1476b8758fd0866d8f
acria-poseidon <ka100899@gmail.com>
2021-04-10 21:46:48 +0400

    added password encryption/decryption and parameter --production from command line

--- oracle-node/acria-oracle-node.js
+++ oracle-node/acria-oracle-node.js
@@ -3,10 +3,16 @@
 // it make its work writing the blockchain with the new data requested.
 const { ApiPromise, WsProvider } = require('@polkadot/api');    
 const { Keyring } = require('@polkadot/api');
-const {mnemonicGenerate,mnemonicToMiniSecret,naclKeypairFromSeed,naclDecrypt,naclEncrypt,} = require('@polkadot/util-crypto');
+const {mnemonicGenerate,mnemonicToMiniSecret,naclKeypairFromSeed,naclDecrypt,naclEncrypt} = require('@polkadot/util-crypto');
 const { u8aToHex,stringToU8a,u8aToString,hexToU8a,isHex } =require('@polkadot/util');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const Base64 =require('crypto-js/enc-base64');
+const Utf8 = require('crypto-js/enc-utf8');
+const crypto = require('crypto');
+let readlineSync = require('readline-sync'); // library to read keyboard input from command line
+const { Console } = require('console');
+const process = require('process');
 
 // customization section - you can change the following constants upon your preferences
 const wsProvider = new WsProvider('ws://127.0.0.1:9944');
@@ -15,34 +21,100 @@ const KEYFILE="./acria-oracle-node.key";            // where to store the keys
 const LOGFILE="./acria-oracle-node.log";            //log file name
 // end customization section
 
-// we launch the main loop in async
+
 console.log("[Info] Acria Oracle Node v. 1.00 - Starting");
-mainloop();
+// check for parameters from command line, default is development mode for production: --production
+let devmode = true;
+if(process.argv.length>1){
+   if(process.argv[2]=="--production"){
+       devmode=false;
+   }
+   console.log(process.argv);
+}
+// we launch the main loop in async
+mainloop(devmode); // jum to async function to use await
 
 // main loop
-async function mainloop(){
+async function mainloop(devmode){
+
     
     const api = await ApiPromise.create({ provider: wsProvider });      // create API object
+    let secretseed="";
     // generate local keys if not yet done
     if(!fs.existsSync(KEYFILE) ) {
-        
+        let password = readlineSync.question('Please insert a new password: ');
+        let password2 = readlineSync.question('Please insert the same password: ');
+        if( password != password2){
+            console.log("[Error] Passwords are not matching,closing the program.");
+            process.exit(1) 
+        }
+        if(password.length<8){
+            console.log("[Error] Passwords must be minimum 8 chars");
+            process.exit(1) 
+        }
         console.log("[Info] Generating Key Pairs");
         //**  FOR PRODUCTION
-        //const randomseed = mnemonicGenerate(24);
-        //**  FOR DEVELOPMENT we use Alice account
-        const randomseed = 'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice';
+        let randomseed="";
+        console.log("devmode:",devmode)
+        if (devmode===false){
+            randomseed = mnemonicGenerate(24);
+        }
+        else {
+            //**  FOR DEVELOPMENT we use Alice account
+            randomseed = 'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice';
+        }
+        secretseed=randomseed;
+        // password derivation -> 256 bit hash
+        const hashSECRET = crypto.createHash('sha256');
+        hashSECRET.on('readable', () => {
+            const data = hashSECRET.read();
+        });
+        hashSECRET.write(password);
+        hashSECRET.end();
+        let SECRETSHA256=hashSECRET.digest();
+        // encrypt the randomseed
+        const { encrypted, nonce } = naclEncrypt(stringToU8a(randomseed), SECRETSHA256);
+        let de = Buffer.from(encrypted);
+        let d64 = de.toString('base64');
+        let n = Buffer.from(nonce);
+        let n64 = n.toString('base64');
+        let randomseedencb64=n64+"###"+d64;
+        console.log("****************** ATTENTION ***********************");
         console.log(`[Info] Generated mnemonic seed: ${randomseed}`);
-        fs.appendFileSync(KEYFILE, randomseed, 'utf8');
+        console.log("Please store in a safe place.");
+        console.log("****************** ATTENTION ***********************");
+        fs.appendFileSync(KEYFILE, randomseedencb64, 'utf8');
+    }else {
+        // request password
+        let password = readlineSync.question('Please insert your password: ');
+        if(password.length==0){
+            console.log("[Error] Passwords cannot be empty.");
+            process.exit(1) 
+        }
+        //password derivation to 256 bits
+        const hashSECRET = crypto.createHash('sha256');
+        hashSECRET.on('readable', () => {
+            const data = hashSECRET.read();
+        });
+        hashSECRET.write(password);
+        hashSECRET.end();
+        let SECRETSHA256=hashSECRET.digest();
+        // Load secret seed from file
+        let secretseedencb64= fs.readFileSync(KEYFILE, 'utf8');
+        let b = secretseedencb64.split('###');
+        let nonceb64=b[0];
+        let encseedb64=b[1];
+        // decrypt secret seed
+        const nonce = Buffer.from(nonceb64,'base64');
+        const encseed = Buffer.from(encseedb64,'base64');
+        let secretseedu8 = naclDecrypt(encseed, nonce, SECRETSHA256);
+        secretseed =u8aToString(secretseedu8);
+        if(secretseed.length==0){
+            console.log("[Error] Password is wrong.");
+            process.exit(1) 
+        }
     }
-    // Load secret seed
-    let secretseed="";
-    try {
-        secretseed= fs.readFileSync(KEYFILE, 'utf8')
-    } catch (err) {
-        console.log("[Error] " +err);
-        process.exit(1);
-    }
-    // generating key ring
+    // generating key ring from secret seed
     const username="AON"
     const keyring = new Keyring({ type: 'sr25519' });
     console.log("[Info] generating key ring from secret seed");
@@ -50,7 +122,6 @@ async function mainloop(){
     const aonAccountId=`${keysPair.address}`;
     console.log("[Info] Account Id of this node: "+aonAccountId);
     
-    //console.log(keyspair);
     // check for configuration file
     if(!fs.existsSync(CONFILE)){
         console.log("[Error] Configuration file not found: "+CONFILE);
